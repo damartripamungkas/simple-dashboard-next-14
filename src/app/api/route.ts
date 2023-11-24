c72f4bd2cc4d28f5b31b45c3dec6f17d44b15de8
damartripamungkas <damartripamungkas@github.com>
2023-11-25 04:44:52 +0700

    chore: remake jsonrpc api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,72 +1,78 @@
 import { Op } from "sequelize"
-import initDb from "~/database/init"
 import modelsProduk from "~/database/models/produk"
 import modelsStatus from "~/database/models/status"
+import createTable from "~/database/migrations/createTable"
+import addDataTable from "~/database/seeders/addDataTable"
 
 const sendRes = (...args: any[]) => Response.json({ result: args[0] }, { status: 200 })
 const sendErr = (...args: any[]) => Response.json({ error: args[0] }, { status: 200 })
+const handle = {
+  hello: async (params: {}) => {
+    return sendRes("hello world")
+  },
+  initDb: async (params: {}) => {
+    await createTable() // create columns table (force)
+    await addDataTable() // add data to table
+    return sendRes({})
+  },
+  tambahDataProduk: async (params: {}) => {
+    await modelsProduk.create(params)
+    return sendRes(true)
+  },
+  bacaDataProduk: async (params: { id: any[] }) => {
+    let opt = {}
+    if (params.id.length != 0) {
+      opt = {
+        where: {
+          id: {
+            [Op.in]: params.id
+          }
+        }
+      }
+    }
+
+    const result = await modelsProduk.findAll({ raw: true, ...opt })
+    return sendRes(result)
+  },
+  bacaDataProdukWithStatus: async (params: { nama_status: string[] }) => {
+    const opt = {
+      include: {
+        model: modelsStatus,
+        attributes: [],
+        where: {
+          nama_status: {
+            [Op.like]: params.nama_status
+          }
+        }
+      }
+    }
+
+    const result = await modelsProduk.findAll({ raw: true, ...opt })
+    return sendRes(result)
+  },
+  editDataProduk: async (params: { data: {}; id: string | number }) => {
+    await modelsProduk.update(params.data, {
+      where: {
+        id: params.id
+      }
+    })
+    return sendRes(true)
+  },
+  hapusDataProduk: async (params: { id: string | number }) => {
+    await modelsProduk.destroy({
+      where: {
+        id: params.id
+      }
+    })
+    return sendRes(true)
+  }
+}
+
 export const POST = async (req: Request) => {
   const { method, params } = await req.json()
   try {
-    if (method == "initDb") {
-      const result = await initDb()
-      return sendRes(result)
-    }
-
-    if (method == "tambahDataProduk") {
-      await modelsProduk.create(params)
-      return sendRes(true)
-    }
-
-    if (method == "bacaDataProduk") {
-      let opt = {}
-      if (params.id.length != 0) {
-        opt = {
-          where: {
-            id: {
-              [Op.in]: params.id
-            }
-          }
-        }
-      }
-
-      const result = await modelsProduk.findAll({ raw: true, ...opt })
-      return sendRes(result)
-    }
-
-    if (method == "bacaDataProdukWithStatus") {
-      const opt = {
-        include: {
-          model: modelsStatus,
-          attributes: [],
-          where: {
-            nama_status: {
-              [Op.like]: params.nama_status
-            }
-          }
-        }
-      }
-
-      const result = await modelsProduk.findAll({ raw: true, ...opt })
-      return sendRes(result)
-    }
-
-    if (method == "editDataProduk") {
-      await modelsProduk.update(params.data, {
-        where: {
-          id: params.id
-        }
-      })
-      return sendRes(true)
-    }
-
-    if (method == "hapusDataProduk") {
-      await modelsProduk.destroy({
-        where: {
-          id: params.id
-        }
-      })
-      return sendRes(true)
+    if (method in handle) {
+      return await handle[method as keyof typeof handle](params)
     }
 
     return sendErr({ code: -32600, message: "invalid request method" })
